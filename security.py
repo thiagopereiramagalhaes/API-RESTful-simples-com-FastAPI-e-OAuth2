454eb16b403cece6fb53c22b7753b6a00d42c436
Thiago Pereira Magalhães <magalhaes.thiagopm@outlook.com>
2026-04-07 20:10:34 +0000

    modified:   .gitignore 	new file:   security.py

---
+++ security.py
@@ -0,0 +1,40 @@
+"""
+Security é um módulo que lida com a autenticação e autorização dos usuários. 
+Ele inclui funções para hash de senhas, verificação de senhas e criação de tokens JWT para autenticação. 
+Ele também define um esquema OAuth2 para integração com o FastAPI.
+"""
+
+from datetime import datetime, timedelta
+from passlib.context import CryptContext
+from jose import JWTError, jwt
+from fastapi.security import OAuth2PasswordBearer
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Carrega as variáveis de ambiente do arquivo .env   
+
+# Configurações do JWT
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+# Contexto para hash de senhas
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# Esquema do OAuth2 (O FastAPI usará isso para criar o botão de Auth no Swagger)
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def verificar_senha(senha_plana, senha_com_hash):
+    # Função para verificar a senha
+    return pwd_context.verify(senha_plana, senha_com_hash)
+
+def obter_hash_senha(senha):
+    # Função para obter o hash da senha
+    return pwd_context.hash(senha)
+
+def criar_token_acesso(dados: dict):
+    # Função para criar o token de acesso
+    dados_para_codificar = dados.copy()
+    expiracao = datetime.now(datetime.timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    dados_para_codificar.update({"exp": expiracao})
+    return jwt.encode(dados_para_codificar, SECRET_KEY, algorithm=ALGORITHM)
